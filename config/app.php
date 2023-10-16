926b5a4e4148c0dfab90a5f4b0339fcce2a2331f
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-10-16 11:48:29 +0100

    fixing depency error

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Telegram\Bot\Laravel\TelegramServiceProvider::class
     ])->toArray(),
 
     /*
