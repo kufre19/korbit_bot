87b3f8c6844f4ad5507f7cea4807228cf8f4872d
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-08 13:49:07 +0100

    fix msg formattubg

--- config/messages.php
+++ config/messages.php
@@ -3,38 +3,25 @@
 
 return [
     "about_us" => <<<MSG
-    Korbit Arbitrage scanner provides crypto traders with an opportunity to capitalize on price differences across multiple 
-    trading platforms. By aggregating market data from numerous crypto exchanges, this app offers valuable insights on optimal
-    buying and selling points. Traders can leverage this information to identify opportunities to buy at low prices and sell 
-    at high prices, maximizing their potential profits.
+    Korbit Arbitrage scanner provides crypto traders with an opportunity to capitalize on price differences across multiple trading platforms. By aggregating market data from numerous crypto exchanges, this app offers valuable insights on optimal buying and selling points. Traders can leverage this information to identify opportunities to buy at low prices and sell at high prices, maximizing their potential profits.
     
-    Our innovative bot enables users to profit from price differences between exchanges without the need to hold tokens in 
-    advance. By leveraging real-time market data, our bot identifies favorable trading opportunities, allowing users to execute
-    trades and capitalize on price differences for maximum profitability. This approach eliminates the need for upfront token 
-    investments, providing a flexible and efficient arbitrage solution for traders.
-    
-    The principle is very simple: you purchase a coin on one exchange and transfer it to another where the price is higher. Once
-    the coin arrives at the second exchange, you can sell it and generate a profit from the price differential. By leveraging 
-    this simple strategy, our bot enables users to take advantage of market inefficiencies and maximize their potential earnings 
-    through arbitrage trading.
-    
-    Currently, the biggest differences are between different blockchains, Other websites or bots do not display this difference, 
-    making this the goldmine and the perfect opportunity to make some serious profits.
-    
-    Our Cryptoscanner operates in real-time, ensuring that you can identify and focus on the cryptocurrencies that are most 
-    relevant to your trading strategy. Our platform aggregates data from more than 65 exchanges, providing you with a 
-    comprehensive overview of the market, all conveniently accessible in one place. This streamlined approach saves you time 
-    and effort, allowing you to make informed trading decisions efficiently.
-    
-     WHAT SETS OUR BOT APART?
+    Our innovative bot enables users to profit from price differences between exchanges without the need to hold tokens in advance. By leveraging real-time market data, our bot identifies favorable trading opportunities, allowing users to execute trades and capitalize on price differences for maximum profitability. This approach eliminates the need for upfront token investments, providing a flexible and efficient arbitrage solution for traders.
+        
+    The principle is very simple: you purchase a coin on one exchange and transfer it to another where the price is higher. Once the coin arrives at the second exchange, you can sell it and generate a profit from the price differential. By leveraging this simple strategy, our bot enables users to take advantage of market inefficiencies and maximize their potential earnings through arbitrage trading.
+        
+    Currently, the biggest differences are between different blockchains, Other websites or bots do not display this difference, making this the goldmine and the perfect opportunity to make some serious profits.
+        
+    Our Cryptoscanner operates in real-time, ensuring that you can identify and focus on the cryptocurrencies that are most relevant to your trading strategy. Our platform aggregates data from more than 65 exchanges, providing you with a comprehensive overview of the market, all conveniently accessible in one place. This streamlined approach saves you time and effort, allowing you to make informed trading decisions efficiently.
+        
+         WHAT SETS OUR BOT APART?
     1️⃣ Multi-Exchange Analysis: Our bot monitors multiple exchanges, ensuring you never miss out on profitable price disparities.
-    
+        
     2️⃣ Real-Time Notifications: Receive instant alerts when potential arbitrage opportunities arise, allowing you to act quickly and secure your profits.
-    
+        
     3️⃣ User-Friendly Interface: Our bot is designed with simplicity in mind, making it easy for both beginners and experienced traders to navigate and utilize its powerful features.
-    
+        
     4️⃣ Speed and Efficiency: Our bot is built with cutting-edge technology, allowing it to swiftly analyze vast amounts of data and identify arbitrage opportunities in real-time. You'll be one step ahead of the competition, maximizing your chances of securing profitable trades.
-    
+        
     5️⃣ Extensive Coin Coverage: Our bot covers a wide range of cryptocurrencies, so you can explore arbitrage opportunities across various digital assets. From Bitcoin to altcoins, we've got you covered.
     
     6️⃣ Continuous Improvements: We believe in constant innovation and regularly update our bot with new features and enhancements. As the cryptocurrency market evolves, our bot evolves with it, keeping you at the forefront of the industry.
@@ -46,28 +33,27 @@ return [
     
     BLOCKCHAINS
     ETHEREUM | BNB | polygon | Optimism | GNOSIS | AVALANCHE | ARBITRUM | FAMTOM | Klatyn | Aurora |
+    
+
     MSG,
     
     "get_trained" => <<<MSG
-    Gain access to a million-dollar opportunity and receive personalized one-on-one training from the experts at KorbitBot 
-    Arbitrage.
+    Gain access to a million-dollar opportunity and receive personalized one-on-one training from the experts at KorbitBot Arbitrage.
     
     Our one-on-one training is a great way to learn what has been hidden for years by the crypto gurus. 
-    
-    For our clients, we have a private chat available during their use of the service where experienced arbitrageurs are 
-    available to assist and share interesting earning strategies.
-    
-    We have been actively engaged in the world of arbitrage since 2015, accumulating substantial experience and witnessing 
-    countless instances of successful ventures. This extensive background has motivated us to develop a comprehensive training 
-    course tailored to equip you with the necessary skills and strategies. We are thrilled to offer this valuable course at a 
-    modest price, ensuring accessibility for aspiring arbitrage enthusiasts like yourself.
-    
+       
+    For our clients, we have a private chat available during their use of the service where experienced arbitrageurs are available to assist and share interesting earning strategies.
+        
+    We have been actively engaged in the world of arbitrage since 2015, accumulating substantial experience and witnessing countless instances of successful ventures.
+    This extensive background has motivated us to develop a comprehensive training course tailored to equip you with the necessary skills and strategies. We are thrilled to offer this valuable course at a modest price, ensuring accessibility for aspiring arbitrage enthusiasts like yourself.
+        
     Gaining access to our one-on-one training is a great way to skyrocket your finance and probably say bye to holding volatile cryptocurrencies on long-term. 
+        
+        - Fee: $300
+        
+        - How to pay: 
+        Step 1: Copy the wallet address below and pay with any exchange or wallet.
+        Step 2: After payment, check your email address for further instructions.
     
-    - Fee: $300
-    
-    - How to pay: 
-    Step 1: Copy the wallet address below and pay with any exchange or wallet.
-    Step 2: After payment, check your email address for further instructions.
     MSG
 ];
