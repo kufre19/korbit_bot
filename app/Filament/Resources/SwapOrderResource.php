48c72dff908a13dc58d00576f7ebbf1edae52344
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-14 16:51:43 +0100

    sal for swap orders

--- app/Filament/Resources/SwapOrderResource.php
+++ app/Filament/Resources/SwapOrderResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\TransactionLogResource\Pages;
-use App\Filament\Resources\TransactionLogResource\RelationManagers;
-use App\Models\TransactionLog;
+use App\Filament\Resources\SwapOrderResource\Pages;
+use App\Filament\Resources\SwapOrderResource\RelationManagers;
+use App\Models\SwapOrder;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -13,9 +13,9 @@ use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class TransactionLogResource extends Resource
+class SwapOrderResource extends Resource
 {
-    protected static ?string $model = TransactionLog::class;
+    protected static ?string $model = SwapOrder::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
@@ -23,9 +23,11 @@ class TransactionLogResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('user_id')
-                    ->relationship('user', 'name')
+                Forms\Components\TextInput::make('user_id')
                     ->required(),
+                Forms\Components\TextInput::make('order_id')
+                    ->required()
+                    ->maxLength(255),
                 Forms\Components\TextInput::make('from_asset')
                     ->required()
                     ->maxLength(255),
@@ -34,8 +36,9 @@ class TransactionLogResource extends Resource
                     ->maxLength(255),
                 Forms\Components\TextInput::make('amount')
                     ->required(),
-                Forms\Components\TextInput::make('received_amount')
-                    ->required(),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255),
             ]);
     }
 
@@ -43,11 +46,12 @@ class TransactionLogResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user.email'),
+                Tables\Columns\TextColumn::make('user_id'),
+                Tables\Columns\TextColumn::make('order_id'),
                 Tables\Columns\TextColumn::make('from_asset'),
                 Tables\Columns\TextColumn::make('to_asset'),
                 Tables\Columns\TextColumn::make('amount'),
-                Tables\Columns\TextColumn::make('received_amount'),
+                Tables\Columns\TextColumn::make('status'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
@@ -67,16 +71,16 @@ class TransactionLogResource extends Resource
     public static function getRelations(): array
     {
         return [
-            // aad
+            //
         ];
     }
     
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListTransactionLogs::route('/'),
-            'create' => Pages\CreateTransactionLog::route('/create'),
-            'edit' => Pages\EditTransactionLog::route('/{record}/edit'),
+            'index' => Pages\ListSwapOrders::route('/'),
+            'create' => Pages\CreateSwapOrder::route('/create'),
+            'edit' => Pages\EditSwapOrder::route('/{record}/edit'),
         ];
     }    
 }
