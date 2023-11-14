22d8b26d938bb95c90ee0e3113557a0e5e00be24
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-14 22:30:29 +0100

    fixed updating rate with the right rates

--- app/Service/ExchangeRateService.php
+++ app/Service/ExchangeRateService.php
@@ -7,19 +7,33 @@ use App\Models\CurrencyRate;
 
 class ExchangeRateService
 {
+
+    private $rates = [
+        "DAI"=>["min"=>0.9589,"max"=>0.9702],
+        "BUSD"=>["min"=>0.9980,"max"=>1.020],
+        "USDT"=>["min"=>0.9999,"max"=>1.001],
+    ];
     public function updateRates()
     {
         $this->updateCurrencyRate('DAI');
         $this->updateCurrencyRate('BUSD');
+        $this->updateCurrencyRate('USDT');
+
     }
+
+
 
     private function updateCurrencyRate($currency)
     {
         // Fetch the current rate
-        $currentRate = CurrencyRate::where('currency', $currency)->first();
+        $currentRate = CurrencyRate::where('currency', $currency)->first() ;
+        if(!$currentRate)
+        {
+            $currentRate = 0;
+        }
 
         // Generate a new random price
-        $newPrice = $this->generateRandomPrice(0.98, 1.05);
+        $newPrice = $this->generateRandomPrice($this->rates[$currency]['min'], $this->rates[$currency]['max']);
 
         // Update the record with the old and new prices
         CurrencyRate::updateOrCreate(
@@ -36,9 +50,11 @@ class ExchangeRateService
         return rand($min * 10000, $max * 10000) / 10000;
     }
 
+    
+
     public function getAssetPrices()
     {
-        
+
     }
 }
 
