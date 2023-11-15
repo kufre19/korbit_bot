0d07a7ac786aab5cd64030f451a4cbdbd9bfdf6d
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-11-15 15:57:03 +0100

    sal

--- app/Service/ArbitrageCalculatorService.php
+++ app/Service/ArbitrageCalculatorService.php
@@ -40,7 +40,7 @@ class ArbitrageCalculatorService implements ServiceInterface
                 $profits ="LIVE SCAN RESULTS FOR PROFITS:" . "\n";
                 foreach ($assets as $asset => $rate) {
                     $value  = $exchangeService->exchangeValuesForDollars($asset,$amount);
-                    $profits .= "**{$asset}: $value **" . "\n" ;
+                    $profits .= "<b>{$asset}: $value </b>" . "\n" ;
                 }
                 $this->telegram_bot->sendMessageToUser($user_id,$profits);
                 $user_session->endSession();
