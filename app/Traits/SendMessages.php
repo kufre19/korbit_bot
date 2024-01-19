a631a7f05aa00a020d5fddd8a26db0064ad6578f
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-01-19 17:10:30 +0100

    fixed 43,44

--- app/Traits/SendMessages.php
+++ app/Traits/SendMessages.php
@@ -83,8 +83,10 @@ trait SendMessages
     public function HelloMessage($username)
     {
         $txt = <<<MSG
-        Hello! Welcome $username, I'm Korbit arbitrage Bot. A machine learning model trained on Kalman filter and least square regression algorithms to help you detect volatility that exists on CEXs and DEXs and then make API calls between the crypto exchanges and via the aid of my sophisticated algorithms detects extremely minute fluctuations and errors up to 4 digits in decimals with lowest graduation up to $0.0049 that exists between top exchanges.
-        Support 60+ CEXs and DEXs some of which are:
+        Hello Welcome $username! 👋
+        I am Korbit Arbitrage Bot, A software designed in helping you navigate the world of crypto trading arbitrage.🌐💸 Trained on Kalman filter and least square regression algorithms, I specialize in detecting volatility on both CEXs and DEXs. My capabilities extend to making API calls between crypto exchanges, leveraging sophisticated algorithms to pinpoint extremely minute fluctuations and errors, even up to 4 digits in decimals. 📊💡I excel in identifying these nuances with the lowest graduation reaching as fine as $0.0049. 🤖💹
+    
+        Supporting 60+ CEXs and DEXs some of which are:
             🔰 www.binance.com
             🔰 www.huobi.com
             🔰 www.CEX.io 
@@ -98,12 +100,17 @@ trait SendMessages
             🔰 www.kucoin.com
             🔰 www.upbit.com
             And many others...
-        Disclaimer: While I aim to provide accurate and helpful information and assists, I have been trained to not accept DEPOSITS, but only to assist you in making API calls through backlog testing between various exchanges.
-        It is important to NOTE that I have been directly deployed on the Blockchain with smart contract technology and therefore have 100% autonomy with no central control. All API calls and transactions between exchanges are recorded on the blockchain and are therefore immutable with 100% transparency and security. So, at all times all funds are within your control.
-        🔰 Detailed article: 
-        🔰 For support, refer to the Manual guide: 
-        You can proceed to select any command from the menu provided below:
+    
+        🚨 Disclaimer: While I aim to provide accurate and helpful information and assists, I have been trained to not accept DEPOSITS, but only to assist you in making API calls through backlog testing between various exchanges. 🚫💰
+        It is important to NOTE that I have been directly deployed on the Blockchain with smart contract technology and therefore have 100% autonomy with no central control. All API calls and transactions between exchanges are recorded on the blockchain and are therefore immutable with 100% transparency and security. So, at all times, all funds are within your control. 🌐🔒💸
+    
+        🔰 Detailed article: [Link] 🔰 
+    
+        For support, refer to the Manual guide: [Link]
+    
+        You can proceed to select any command from the menu provided below: 📲👇
         MSG;
+    
 
         return $txt;
     }
