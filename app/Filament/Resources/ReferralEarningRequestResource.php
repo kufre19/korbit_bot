faab2905cadf01551c330584e2f2b2051772d234
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-02-03 17:28:12 +0100

    sal

--- app/Filament/Resources/ReferralEarningRequestResource.php
+++ app/Filament/Resources/ReferralEarningRequestResource.php
@@ -51,7 +51,7 @@ class ReferralEarningRequestResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('user.tg_id')->label('User TG ID'),
                 Tables\Columns\TextColumn::make('usdt_address')->label('USDT Address'),
-                Tables\Columns\TextColumn::make('user.wallet.referral_balance')->label('Referral Balance'),
+                // Tables\Columns\TextColumn::make('user.wallet.referral_balance')->label('Referral Balance'),
                 Tables\Columns\TextColumn::make('status')->label('Request Status'),
                 Tables\Columns\TextColumn::make('created_at')->dateTime()->label('Created At'),
                 Tables\Columns\TextColumn::make('updated_at')->dateTime()->label('Updated At'),
