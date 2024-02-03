992557b2b963e0b5ee464abd071cab47f48974b8
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-02-03 16:47:27 +0100

    sal

--- app/Models/ReferralEarningRequest.php
+++ app/Models/ReferralEarningRequest.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReferralEarningRequest extends Model
 {
     use HasFactory;
     protected $table = "referral_earnings_requests";
     protected $fillable = ['user_id', 'usdt_address'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,"user_id");
+    }
 
 }
