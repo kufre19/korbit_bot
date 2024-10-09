064a879b9a48143d242d359b444cc7feb976a23e
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-10-09 21:06:55 +0100

    added contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,286 @@
+@extends('layouts.web')
+
+@section('header-style')
+<body
+	class="page-template-default page page-id-163 wp-custom-logo wp-embed-responsive slideout-enabled slideout-mobile sticky-menu-fade sticky-enabled desktop-sticky-menu no-sidebar nav-float-right one-container header-aligned-left dropdown-hover full-width-content"
+	itemtype="https://schema.org/WebPage" itemscope="">
+	<a class="screen-reader-text skip-link" href="#content" title="Skip to content">Skip to content</a>
+
+    
+            {{-- header  --}}
+            @include('layouts.header')
+	<div class="gb-container gb-container-ef1aae2b">
+		<div class="gb-container gb-container-956de9c7">
+
+			<h1 class="gb-headline gb-headline-f80518be gb-headline-text">Got Questions?</h1>
+
+		</div>
+	</div>
+	<div class="site grid-container container hfeed" id="page">
+		<div class="site-content" id="content">
+
+			<div class="content-area" id="primary">
+				<main class="site-main" id="main">
+
+					<article id="post-163" class="post-163 page type-page status-publish"
+						itemtype="https://schema.org/CreativeWork" itemscope="">
+						<div class="inside-article">
+
+							<header class="entry-header" aria-label="Content">
+								<h1 class="entry-title" itemprop="headline">Got Questions?</h1>
+							</header>
+
+
+							<div class="entry-content" itemprop="text">
+								<section class="gb-container gb-container-59128a37">
+									<div class="gb-container gb-container-2a36c822">
+
+										<h2 class="gb-headline gb-headline-6907d725 gb-headline-text">Drop us a Message
+										</h2>
+
+
+
+										<p>If you have questions, feel free to drop us a message, and we will get back
+											to you within 2 to 3 business days.</p>
+
+
+										<div class="gb-container gb-container-5b4ac1a6">
+											<div class="forminator-ui forminator-custom-form forminator-custom-form-1250 forminator-design--default  forminator_ajax"
+												data-forminator-render="0" data-form="forminator-module-1250"
+												data-uid="67002e4d1be44"><br></div>
+											<form id="forminator-module-1250"
+												class="forminator-ui forminator-custom-form forminator-custom-form-1250 forminator-design--default  forminator_ajax"
+												method="post" data-forminator-render="0" data-form-id="1250"
+												data-design="default" data-grid="open" style="display: none;"
+												data-uid="67002e4d1be44">
+												<div role="alert" aria-live="polite"
+													class="forminator-response-message forminator-error"
+													aria-hidden="true"></div>
+												<div class="forminator-row">
+													<div id="name-1"
+														class="forminator-field-name forminator-col forminator-col-12 ">
+														<div class="forminator-field"><label
+																for="forminator-field-name-1_67002e4d1be44"
+																class="forminator-label"
+																id="forminator-field-name-1_67002e4d1be44-label">First
+																Name <span
+																	class="forminator-required">*</span></label><input
+																type="text" name="name-1" value=""
+																placeholder="E.g. John"
+																id="forminator-field-name-1_67002e4d1be44"
+																class="forminator-input forminator-name--field"
+																aria-required="true"></div>
+													</div>
+												</div>
+												<div class="forminator-row">
+													<div id="email-1"
+														class="forminator-field-email forminator-col forminator-col-12 ">
+														<div class="forminator-field"><label
+																for="forminator-field-email-1_67002e4d1be44"
+																class="forminator-label"
+																id="forminator-field-email-1_67002e4d1be44-label">Email
+																Address <span
+																	class="forminator-required">*</span></label><input
+																type="email" name="email-1" value=""
+																placeholder="E.g. john@doe.com"
+																id="forminator-field-email-1_67002e4d1be44"
+																class="forminator-input forminator-email--field"
+																data-required="true" aria-required="true"></div>
+													</div>
+												</div>
+												<div class="forminator-row">
+													<div id="textarea-1"
+														class="forminator-field-textarea forminator-col forminator-col-12 ">
+														<div class="forminator-field"><label
+																for="forminator-field-textarea-1_67002e4d1be44"
+																id="forminator-field-textarea-1_67002e4d1be44-label"
+																class="forminator-label">Message</label><textarea
+																name="textarea-1" placeholder="Enter your message..."
+																id="forminator-field-textarea-1_67002e4d1be44"
+																class="forminator-textarea" rows="6"
+																style="min-height:140px;"
+																maxlength="180"></textarea><span
+																id="forminator-field-textarea-1_67002e4d1be44-description"
+																class="forminator-description"><span data-limit="180"
+																	data-type="characters">0 / 180</span></span></div>
+													</div>
+												</div>
+												<div class="forminator-row">
+													<div id="captcha-1"
+														class="forminator-field-captcha forminator-col forminator-col-12 ">
+														<div class="forminator-g-recaptcha recaptcha-invisible"
+															data-theme="light" data-badge="bottomright"
+															data-sitekey="6LdRRyAqAAAAACDXK7s5Dpf1sxrPmz8P1lVFBTil"
+															data-size="invisible"></div>
+													</div>
+												</div><input type="hidden" name="referer_url" value="">
+												<div class="forminator-row forminator-row-last">
+													<div class="forminator-col">
+														<div class="forminator-field"><button
+																class="forminator-button forminator-button-submit">Send
+																Message</button></div>
+													</div>
+												</div><input type="hidden" id="forminator_nonce" name="forminator_nonce"
+													value="7dd7e8e2ab"><input type="hidden" name="_wp_http_referer"
+													value="/contact/"><input type="hidden" name="form_id"
+													value="1250"><input type="hidden" name="page_id" value="163"><input
+													type="hidden" name="form_type" value="default"><input type="hidden"
+													name="current_url" value="https://www.arbihunt.com/contact/"><input
+													type="hidden" name="render_id" value="0"><input type="hidden"
+													name="action" value="forminator_submit_form_custom-forms"><label
+													for="input_4" class="forminator-hidden" aria-hidden="true">Please do
+													not fill in this field. <input id="input_4" type="text"
+														name="input_4" value="" autocomplete="off"></label>
+											</form>
+
+										</div>
+									</div>
+
+									<div class="gb-container gb-container-3ad0dd62">
+										<div class="gb-container gb-container-98d407db">
+
+											<h3 class="gb-headline gb-headline-641c0722 gb-headline-text"><strong>We
+													would like to hear from you!</strong></h3>
+
+
+
+											<p>Address: 1309 Coffeen Avenue, Ste 1200<br>Sheridan, WY 82801, USA</p>
+
+
+
+											<p class="gb-headline gb-headline-f784d25e gb-headline-text">Email: <a
+													href="mailto:hello@arborist.com">contact@arbihunt.com</a></p>
+
+										</div>
+									</div>
+								</section>
+
+								<section class="gb-container gb-container-9424b461"></section>
+							</div>
+
+						</div>
+					</article>
+				</main>
+			</div>
+
+
+		</div>
+	</div>
+
+
+        {{-- footer --}}
+        @include('layouts.footer')
+
+
+	<nav id="generate-slideout-menu" class="main-navigation slideout-navigation"
+		itemtype="https://schema.org/SiteNavigationElement" itemscope="">
+		<div class="inside-navigation grid-container grid-parent">
+			<button class="slideout-exit has-svg-icon"><span class="gp-icon pro-close">
+					<svg viewBox="0 0 512 512" aria-hidden="true" role="img" version="1.1"
+						xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1em"
+						height="1em">
+						<path
+							d="M71.029 71.029c9.373-9.372 24.569-9.372 33.942 0L256 222.059l151.029-151.03c9.373-9.372 24.569-9.372 33.942 0 9.372 9.373 9.372 24.569 0 33.942L289.941 256l151.03 151.029c9.372 9.373 9.372 24.569 0 33.942-9.373 9.372-24.569 9.372-33.942 0L256 289.941l-151.029 151.03c-9.373 9.372-24.569 9.372-33.942 0-9.372-9.373-9.372-24.569 0-33.942L222.059 256 71.029 104.971c-9.372-9.373-9.372-24.569 0-33.942z">
+						</path>
+					</svg>
+				</span> <span class="screen-reader-text">Close</span></button>
+			<div class="main-nav">
+				<ul id="menu-main-1" class=" slideout-menu">
+					<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-383"><a
+							href="https://www.arbihunt.com/">Home</a></li>
+					<li
+						class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-163 current_page_item menu-item-385">
+						<a href="https://www.arbihunt.com/contact/" aria-current="page">Got Questions?</a></li>
+				</ul>
+			</div>
+			<a class="gb-button gb-button-3e53fdf1 gb-button-text" href="https://app.arbihunt.com/" target="_blank"
+				rel="noopener noreferrer">LOGIN/SIGNUP</a>
+		</div><!-- .inside-navigation -->
+	</nav><!-- #site-navigation -->
+
+	<div class="slideout-overlay">
+	</div>
+	<script
+		id="generate-a11y">!function () { "use strict"; if ("querySelector" in document && "addEventListener" in window) { var e = document.body; e.addEventListener("mousedown", function () { e.classList.add("using-mouse") }), e.addEventListener("keydown", function () { e.classList.remove("using-mouse") }) } }();</script>
+	<link rel="stylesheet" id="forminator-module-css-1250-css" href="css/style-1250.css" media="all">
+	<link rel="stylesheet" id="forminator-icons-css" href="css/forminator-icons.min.css" media="all">
+	<link rel="stylesheet" id="forminator-utilities-css" href="css/forminator-utilities.min.css" media="all">
+	<link rel="stylesheet" id="forminator-grid-default-css" href="css/forminator-grid.open.min.css" media="all">
+	<link rel="stylesheet" id="forminator-forms-default-base-css" href="css/forminator-form-default.base.min.css"
+		media="all">
+	<link rel="stylesheet" id="buttons-css" href="css/buttons.min.css" media="all">
+	<script src="js/sticky.min.js" id="generate-sticky-js"></script>
+	<script id="generate-offside-js-extra">
+		var offSide = { "side": "left" };
+	</script>
+	<script src="js/offside.min.js" id="generate-offside-js"></script>
+	<!--[if lte IE 11]>
+<script src="https://www.arbihunt.com/wp-content/themes/generatepress/assets/js/classList.min.js?ver=3.3.1" id="generate-classlist-js"></script>
+<![endif]-->
+	<script id="generate-menu-js-extra">
+		var generatepressMenu = { "toggleOpenedSubMenus": "1", "openSubMenuLabel": "Open Sub-Menu", "closeSubMenuLabel": "Close Sub-Menu" };
+	</script>
+	<script src="js/menu.min.js" id="generate-menu-js"></script>
+	<script src="js/jquery-migrate.min.js" id="jquery-migrate-js"></script>
+	<script src="js/jquery.validate.min.js" id="forminator-jquery-validate-js"></script>
+	<script src="js/forminator-form.min.js" id="forminator-form-js"></script>
+	<script id="forminator-front-scripts-js-extra">
+		var ForminatorFront = { "ajaxUrl": "https:\/\/www.arbihunt.com\/wp-admin\/admin-ajax.php", "cform": { "processing": "Submitting form, please wait", "error": "An error occurred while processing the form. Please try again", "upload_error": "An upload error occurred while processing the form. Please try again", "pagination_prev": "Previous", "pagination_next": "Next", "pagination_go": "Submit", "gateway": { "processing": "Processing payment, please wait", "paid": "Success! Payment confirmed. Submitting form, please wait", "error": "Error! Something went wrong when verifying the payment" }, "captcha_error": "Invalid CAPTCHA", "no_file_chosen": "No file chosen", "intlTelInput_utils_script": "https:\/\/www.arbihunt.com\/wp-content\/plugins\/forminator\/assets\/js\/library\/intlTelInputUtils.js", "process_error": "Please try again" }, "poll": { "processing": "Submitting vote, please wait", "error": "An error occurred saving the vote. Please try again" }, "quiz": { "view_results": "View Results" }, "select2": { "load_more": "Loading more results\u2026", "no_result_found": "No results found", "searching": "Searching\u2026", "loaded_error": "The results could not be loaded." } };
+	</script>
+	<script src="js/front.multi.min.js" id="forminator-front-scripts-js"></script>
+	<script src="js/api.js" id="forminator-google-recaptcha-js"></script>
+	<script
+		type="text/javascript">jQuery(function () { jQuery.ajax({ url: 'https://www.arbihunt.com/wp-admin/admin-ajax.php', type: "POST", data: { action: "forminator_get_nonce", }, success: function (response) { jQuery('#forminator-module-1250 #forminator_nonce').val(response.data); } }); })</script>
+	<script type="text/javascript">
+		jQuery(function () {
+			window.Forminator_Cform_Paginations = window.Forminator_Cform_Paginations || [];
+			window.Forminator_Cform_Paginations[1250] =
+				{ "has-pagination": false, "pagination-header-design": "show", "pagination-header": "nav", "last-steps": "Finish", "last-previous": "Previous", "pagination-labels": "default", "has-paypal": false };
+
+			var runForminatorFront = function () {
+				jQuery('#forminator-module-1250[data-forminator-render="0"]')
+					.forminatorFront({ "form_type": "custom-form", "inline_validation": true, "print_value": false, "rules": "\"name-1\": \"required\",\"name-1\": \"trim\",\"email-1\": {\n\"required\": true,\"emailWP\": true,},\n\"textarea-1\": {\"maxlength\": 180,},", "messages": "\"name-1\": \"This field is required. Please input your name.\",\n\"email-1\": {\n\"required\": \"This field is required. Please input a valid email.\",\n\"emailWP\": \"This is not a valid email.\",\n\"email\": \"This is not a valid email.\",\n},\n\"textarea-1\": {\"maxlength\": \"You exceeded the allowed amount of characters. Please check again.\",\n},", "conditions": { "fields": [], "relations": { "name-1": [], "email-1": [], "textarea-1": [], "captcha-1": [], "submit": [] } }, "calendar": "{\"days\":[\"Su\",\"Mo\",\"Tu\",\"We\",\"Th\",\"Fr\",\"Sa\"],\"months\":[\"Jan\",\"Feb\",\"Mar\",\"Apr\",\"May\",\"Jun\",\"Jul\",\"Aug\",\"Sep\",\"Oct\",\"Nov\",\"Dec\"]}", "paypal_config": { "live_id": "", "sandbox_id": "", "redirect_url": "https:\/\/www.arbihunt.com\/contact", "form_id": 1250 }, "forminator_fields": ["address", "calculation", "captcha", "consent", "currency", "custom", "date", "email", "gdprcheckbox", "group", "hidden", "html", "checkbox", "name", "number", "page-break", "password", "paypal", "phone", "postdata", "radio", "section", "select", "stripe", "text", "textarea", "time", "upload", "url"], "general_messages": { "calculation_error": "Failed to calculate field.", "payment_require_ssl_error": "SSL required to submit this form, please check your URL.", "payment_require_amount_error": "PayPal amount must be greater than 0.", "form_has_error": "Please correct the errors before submission." }, "payment_require_ssl": false, "has_loader": true, "loader_label": "Submitting...", "calcs_memoize_time": 300, "is_reset_enabled": true, "has_stripe": false, "has_paypal": false, "submit_button_class": "" });
+			}
+
+			if (window.elementorFrontend) {
+				if (typeof elementorFrontend.hooks !== "undefined") {
+					elementorFrontend.hooks.addAction('frontend/element_ready/global', function () {
+						runForminatorFront();
+					});
+				}
+			} else {
+				runForminatorFront();
+			}
+
+			if (typeof ForminatorValidationErrors !== 'undefined') {
+				var forminatorFrontSubmit = jQuery(ForminatorValidationErrors.selector).data('forminatorFrontSubmit');
+				if (typeof forminatorFrontSubmit !== 'undefined') {
+					forminatorFrontSubmit.show_messages(ForminatorValidationErrors.errors);
+				}
+			}
+			if (typeof ForminatorFormHider !== 'undefined') {
+				var forminatorFront = jQuery(ForminatorFormHider.selector).data('forminatorFront');
+				if (typeof forminatorFront !== 'undefined') {
+					jQuery(forminatorFront.forminator_selector).find('.forminator-row').hide();
+					jQuery(forminatorFront.forminator_selector).find('.forminator-pagination-steps').hide();
+					jQuery(forminatorFront.forminator_selector).find('.forminator-pagination-footer').hide();
+				}
+			}
+			if (typeof ForminatorFormNewTabRedirect !== 'undefined') {
+				var forminatorFront = ForminatorFormNewTabRedirect.url;
+				if (typeof forminatorFront !== 'undefined') {
+					window.open(ForminatorFormNewTabRedirect.url, '_blank');
+				}
+			}
+		});
+	</script>
+
+
+
+
+
+
+
+</body>
+
+@endsection
